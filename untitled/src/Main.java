94a77941ba42c54841b0df945dbc2c4b3fe3a673
kritiadhikari135 <kriteeadhikari90@gmail.com>
2025-11-25 16:11:54 +0545

    prepareStatement

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -7,16 +7,26 @@ public class Main {
         String url = "jdbc:postgresql://localhost:5432/Demo";
         String uname = "postgres";
         String pass = "1234";
-        String sql = "delete from student where s_id = 7";
+//        String sql = "delete from student where s_id = 7";
 
-
+        int s_id = 102;
+        String s_name = "jasmine";
+        int marks = 52;
+//        String sql = "insert into student values (" + s_id + ", '" + s_name +" ', " + marks + "  )";
+        String sql = "insert into student values(?,?,?)";
         //Class.forName("org.postgresql.Driver");
         Connection con = DriverManager.getConnection(url, uname, pass);
-        System.out.println("Connection Established");
-        Statement st = con.createStatement();
-        st.execute(sql);
+//        Statement st = con.createStatement();
+
+        PreparedStatement st = con.prepareStatement(sql);
+        st.setInt(1, s_id);
+        st.setString(2, s_name);
+        st.setInt(3, marks);
+
+        st.execute();
+
         con.close();
-        System.out.println("Connection closed");
+
 
 
 
